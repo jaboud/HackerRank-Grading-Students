d449f563987e8d9be700cbf088fb1c8e5d541fa4
jaboud <jaboud94@gmail.com>
2019-09-27 17:09:32 +1000

    Project creation

---
+++ HackerRank-Grading-Students/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Grading_Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
